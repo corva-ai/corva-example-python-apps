e31c76d0c28a20f1a1a7297f0a2a1e044dab20c3
Oleksii Symon <oleksii.symon@corva.ai>
2021-04-05 11:41:31 +0300

    Synced Settings.app_name and manifest.json

--- task/formation-evaluation-importer/src/configuration.py
+++ task/formation-evaluation-importer/src/configuration.py
@@ -5,7 +5,7 @@ class Settings(pydantic.BaseSettings):
     provider: str = 'big-data-energy'
     collection: str = 'formation-evaluation'
     version: int = 1
-    app_name: str = 'formation-evaluation-importer-task'
+    app_name: str = 'formation-evaluation-importer'
     chunk_size: int = 667
 
     @property
