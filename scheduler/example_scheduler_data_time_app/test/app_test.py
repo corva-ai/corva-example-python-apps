80ae611114b0a2ea65ea1bb7716f43b68a379027
Oleksii Symon <oleksii.symon@corva.ai>
2022-12-19 12:38:37 +0200

    Finished `example_scheduler_data_time_app` test

--- scheduler/example_scheduler_data_time_app/test/app_test.py
+++ scheduler/example_scheduler_data_time_app/test/app_test.py
@@ -1,4 +1,7 @@
-from corva import ScheduledDataTimeEvent
+import unittest.mock
+
+from corva import Api, ScheduledDataTimeEvent
+
 from lambda_function import lambda_handler
 
 
@@ -7,4 +10,19 @@ def test_app(app_runner):
         company_id=1, asset_id=1234, start_time=1578291000, end_time=1578291300
     )
 
-    app_runner(lambda_handler, event=event)
+    with unittest.mock.patch.object(
+        Api, 'get_dataset', return_value=[{'data': {'rop': 15}, 'company_id': 1}]
+    ), unittest.mock.patch.object(Api, 'post') as post_patch:
+        app_runner(lambda_handler, event=event)
+
+    assert post_patch.call_args.kwargs['data'] == [
+        {
+            'timestamp': 1578291300,
+            'asset_id': 1234,
+            'company_id': 1,
+            'provider': 'test-provider',
+            'collection': 'example-scheduled-data-time-app',
+            'data': {'rop': 15, 'start_time': 1578291000, 'end_time': 1578291300},
+            'version': 1,
+        }
+    ]
